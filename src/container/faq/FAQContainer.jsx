71128cff79534b72eab7d9f7770ab0433cc8dfb7
Arjun850610 <arjun@qurilo.com>
2025-01-24 10:57:19 +0530

    Add search functionality to FAQContainer and enhance layout

--- src/container/faq/FAQContainer.jsx
+++ src/container/faq/FAQContainer.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { IoSearch } from "react-icons/io5";
 
 const FAQContainer = ({ data }) => {
   const [selectedCategory, setselectedCategory] = useState("");
   const [filteredFaqs, setfilteredFaqs] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const toggleFaq = (index) => {
     setActiveIndex(activeIndex === index ? null : index);
@@ -17,15 +19,25 @@ const FAQContainer = ({ data }) => {
   }, []);
 
   useEffect(() => {
-    setfilteredFaqs(
-      data?.find((cate) => cate?.categoryName === selectedCategory)?.faqs
-    );
-  }, [selectedCategory]);
+    if (searchTerm !== "") {
+      setfilteredFaqs(
+        data
+          ?.find((cate) => cate?.categoryName === selectedCategory)
+          ?.faqs?.filter((faq) =>
+            faq?.question?.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+      );
+    } else {
+      setfilteredFaqs(
+        data?.find((cate) => cate?.categoryName === selectedCategory)?.faqs
+      );
+    }
+  }, [selectedCategory, searchTerm]);
 
   return (
     <div className="w-full overflow-hidden py-8">
       <div
-        className={`w-full text-nowrap flex items-center overflow-x-auto px-4`}
+        className={`w-full text-nowrap flex items-center md:justify-center gap-2 overflow-x-auto px-4`}
       >
         {data?.map((faq, i) => (
           <button
@@ -34,15 +46,29 @@ const FAQContainer = ({ data }) => {
             className={`px-4 uppercase py-2 transition-custom ${
               selectedCategory === faq?.categoryName
                 ? "border-b-2 border-secondary font-bold text-secondary"
-                : "text-black/40"
+                : "text-black/40 text-sm"
             }`}
           >
             {faq?.categoryName}
           </button>
         ))}
       </div>
-      <div className="bg-secondary/10 px-5 py-8 w-full">
-        <ul>
+      <div className="bg-secondary/10 px-5 py-8 md:px-10 md:py-10 space-y-6 w-full">
+        <div className="relative lg:w-[60%] lg:mx-auto">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full border border-black/10 p-3 rounded px-4 pl-10 focus:outline-secondary transition-custom"
+            placeholder={"Looking for something! Search your query"}
+          />
+
+          <IoSearch
+            className={`text-xl text-gray-300 absolute top-[50%] left-2 translate-y-[-50%] cursor-pointer`}
+          />
+        </div>
+
+        <ul className="flex flex-col gap-4 lg:w-[60%] lg:mx-auto">
           {filteredFaqs?.map((faq, index) => (
             <li
               key={index}
@@ -52,7 +78,9 @@ const FAQContainer = ({ data }) => {
                 onClick={() => toggleFaq(index)}
                 className="flex justify-between items-center w-full font-medium text-lg text-left"
               >
-                <span className="font-[900] text-base">{faq.question}</span>
+                <span className="font-[700] text-base">
+                  {index + 1}. {faq.question}
+                </span>
                 <svg
                   className={`transition-transform ${
                     activeIndex === index ? "-rotate-0" : "-rotate-90"
@@ -75,11 +103,13 @@ const FAQContainer = ({ data }) => {
                 }`}
               >
                 <p className="mt-3 text-base leading-7 text-gray-600">
-                    {Array.isArray(faq?.answer) ? (
-                        faq?.answer?.map((ans, i) => (
-                            <span key={i} className="block w-full" >-{ans}</span>
-                        ))
-                    ) : faq?.answer}
+                  {Array.isArray(faq?.answer)
+                    ? faq?.answer?.map((ans, i) => (
+                        <span key={i} className="block w-full">
+                          -{ans}
+                        </span>
+                      ))
+                    : faq?.answer}
                 </p>
               </div>
             </li>
