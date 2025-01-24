9e996f43c8821efd516243953c8cec19eb42e8ec
Arjun850610 <arjun@qurilo.com>
2025-01-24 18:59:53 +0530

    Add course details page with dynamic routing and JSON data integration; remove old course detail component

---
+++ src/pages/courses/[slug].js
@@ -0,0 +1,41 @@
+import React from "react";
+import path from "path";
+import fs from "fs";
+
+export default function CourseDetailsPage({ pageData }) {
+  return <div>{pageData?.name}</div>;
+}
+
+export async function getStaticPaths() {
+  const dataDir = path.join(process.cwd(), "src/data/courses");
+  console.log("dataDir", dataDir);
+  const files = fs.readdirSync(dataDir);
+  console.log("files", files);
+  const paths = files.map((filename) => ({
+    params: { slug: filename.replace(/\.json$/, "") },
+  }));
+  console.log("paths", paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const dataFilePath = path.join(
+    process.cwd(),
+    "src/data/courses",
+    `${slug}.json`
+  );
+  console.log("dataFilePath", dataFilePath);
+  const rawData = fs.readFileSync(dataFilePath, "utf-8");
+  console.log("rawData", rawData);
+  const pageData = JSON.parse(rawData);
+  return {
+    props: {
+      pageData,
+    },
+  };
+}
