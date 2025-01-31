2fbbf230cf98f0d98d7a311f4fa8e24e169ba74e
Aman Kumar <mr.amankr99@gmail.com>
2025-01-31 18:48:23 +0530

    kjkjk

--- src/pages/admin-dashboard/teacherrequests/index.jsx
+++ src/pages/admin-dashboard/teacherrequests/index.jsx
@@ -1,10 +1,10 @@
-import WithdrawalHistory from '@/components/admin-dashboard/user/user'
+import TeacherRequests from '@/components/admin-dashboard/teacherrequests'
 import StudentDashboardLayout from '@/layouts/student-dashboard/StudentDashboardLayout'
 import React from 'react'
 
 const index = () => {
   return (
-    <StudentDashboardLayout><WithdrawalHistory/></StudentDashboardLayout>
+    <StudentDashboardLayout><TeacherRequests/></StudentDashboardLayout>
   )
 }
 
