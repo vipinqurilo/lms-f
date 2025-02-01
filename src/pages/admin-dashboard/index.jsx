6aa205af71b35f4190f8d5c4f73ad2ebd022c039
kdidrees <Khurshid@qurilo.com>
2025-02-01 13:08:00 +0530

    adding components for admin dashboard

--- src/pages/admin-dashboard/index.jsx
+++ src/pages/admin-dashboard/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import StatesCard from "@/components/admin-dashboard/dashboard/StatesCard";
+import StudentDashboardLayout from "@/layouts/student-dashboard/StudentDashboardLayout";
+import EarningSummary from "@/components/admin-dashboard/dashboard/EarningSummary";
+import BookingTable from "@/components/admin-dashboard/dashboard/Table";
 
 const index = () => {
-  return <StatesCard />;
+  return (
+    <StudentDashboardLayout>
+      <div>
+        <StatesCard />
+        <EarningSummary />
+        <BookingTable/>
+      </div>
+    </StudentDashboardLayout>
+  );
 };
 
 export default index;
