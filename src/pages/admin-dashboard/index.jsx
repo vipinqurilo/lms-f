e0db86bd6bfb0375476c3569f282d180b2214e56
kdidrees <Khurshid@qurilo.com>
2025-02-01 18:08:58 +0530

    update

--- src/pages/admin-dashboard/index.jsx
+++ src/pages/admin-dashboard/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import StatesCard from "@/components/admin-dashboard/dashboard/StatesCard";
 import StudentDashboardLayout from "@/layouts/student-dashboard/StudentDashboardLayout";
 import EarningSummary from "@/components/admin-dashboard/dashboard/EarningSummary";
-import BookingTable from "@/components/admin-dashboard/dashboard/Table";
+import BookingTable from "@/components/admin-dashboard/dashboard/SalesTable";
 
 const index = () => {
   return (
