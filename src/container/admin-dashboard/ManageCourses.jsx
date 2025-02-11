4fcb4f26dad006ce4277b939da0b8c29b92fc702
kdidrees <Khurshid@qurilo.com>
2025-02-11 15:49:50 +0530

    fixes in the manage courses component

--- src/container/admin-dashboard/ManageCourses.jsx
+++ src/container/admin-dashboard/ManageCourses.jsx
@@ -2,65 +2,67 @@
 import InstructorButton from "@/components/instructor/InstructorButton";
 import { Pagination } from "@/components/student-dashboard/Pagination";
 import React, { useEffect, useState } from "react";
-import { FiEdit, FiEdit3 } from "react-icons/fi";
+import { FiEdit3 } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
-import { BiBook, BiCheckCircle, BiTime, BiEdit } from "react-icons/bi";
-import DeleteModal from "@/components/instructor/DeleteModal";
-import { useRouter } from "next/navigation";
-import CreatedCourses from "../instructor/dashboard/CreatedCourses";
-import { getAllIntructorCourses } from "@/store/slices/instructor/courseSlice";
+import { BiBook, BiCheckCircle, BiTime } from "react-icons/bi";
 import Loader from "@/components/common/Loader";
+import {
+  updateAdminCourseStatus,
+  getAllAdminCourses,
+} from "@/store/slices/admin-dashboard/courseSlice";
+import CreatedCourses from "../instructor/dashboard/CreatedCourses";
 
 const tabs = [
-  {
-    icon: <BiBook size={20} />,
-    tab: "All",
-  },
+  // {
+  //   icon: <BiBook size={20} />,
+  //   tab: "All",
+  // },
   {
     icon: <BiTime size={20} />,
-    tab: "Pending",
+    tab: "pending",
   },
   {
     icon: <BiCheckCircle size={20} />,
-    tab: "Approved",
+    tab: "published",
   },
 ];
 
 const ManageCourses = () => {
   const dispatch = useDispatch();
-  const router = useRouter();
-  const { data: courses } = useSelector(
-    (state) => state.admin?.course?.courses
-  ) || { data: [] };
+  const courses =
+    useSelector((state) => state.admin?.course?.courses) || [];
 
   const getloading = useSelector(
     (state) => state.instructor.course.isLoading.getAllIntructorCourses
   );
+
   const [editCourseId, setEditCourseId] = useState(null);
-  const [selecteStatus, setselecteStatus] = useState("All");
-  const [filteredCourses, setfilteredCourses] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState("pending");
   const [currentPage, setCurrentPage] = useState(1);
 
+  console.log(selectedStatus, "selctedkd");
+
+  useEffect(() => {
+    dispatch(getAllAdminCourses({ status: selectedStatus }));
+  }, [dispatch, selectedStatus]);
+
   const handleEditClick = (courseId) => {
-   
-    console.log(courseId,'course id kd')
-    
     setEditCourseId(editCourseId === courseId ? null : courseId);
   };
 
-  useEffect(() => {
-    if (selecteStatus !== "All") {
-      setfilteredCourses(
-        courses?.filter((course) => course.status === selecteStatus)
-      );
-    } else {
-      setfilteredCourses(courses);
-    }
-  }, [selecteStatus]);
+  const handleStatusUpdate = (courseId, newStatus) => {
+    dispatch(updateAdminCourseStatus({ courseId, status: newStatus })).then(
+      () => {
+        setEditCourseId(null);
+      }
+    );
+  };
 
-  const handleStatusChange = (value) => setselecteStatus(value);
+  const handleStatusChange = (value) => {
+    setSelectedStatus(value);
+  };
 
-  const filteredData = filteredCourses?.map((course) => ({
+  const filteredData = courses?.map((course) => ({
     image: course?.courseImage,
     title: course?.courseTitle,
     des: course?.courseDescription,
@@ -69,11 +71,13 @@ const ManageCourses = () => {
       <div className="flex items-center gap-5">
         {editCourseId === course?._id ? (
           <div className="flex items-center gap-5">
-            <select name="" id="" className="px-2 py-1">
-              <option onClick={() => setEditCourseId(null)} value="Pending">
-                Pending
-              </option>
-              <option onClick={() => setEditCourseId(null)} value="Approved">
+            <select
+              className="px-2 py-1 border rounded  bg-transparent border-none"
+              value={course.status}
+              onChange={(e) => handleStatusUpdate(course?._id, e.target.value)}
+            >
+              <option value="pending">Pending</option>
+              <option className="text-green-600" value="published">
                 Approved
               </option>
             </select>
@@ -81,7 +85,7 @@ const ManageCourses = () => {
         ) : (
           <button
             onClick={() => handleEditClick(course?._id)}
-            className="p-1.5 border flex border-black/10 rounded hover:border-green-200 transition-custom hover:text-green-500"
+            className="p-1.5 border  bg-transparent border-none flex border-black/10 rounded hover:border-green-200 transition-custom hover:text-green-500"
           >
             <span className="px-2">{course?.status}</span>
             <FiEdit3 size={20} />
@@ -90,6 +94,8 @@ const ManageCourses = () => {
       </div>
     ),
   }));
+
+  console.log(filteredData, "filtereddatakd");
 
   return (
     <section className="dashboard-sub-container flex flex-col gap-6">
@@ -100,7 +106,7 @@ const ManageCourses = () => {
             tab={tab?.tab}
             icon={tab?.icon}
             condition={`${
-              selecteStatus === tab?.tab && "!bg-secondary text-white"
+              selectedStatus === tab?.tab && "!bg-secondary text-white"
             }`}
             handleClick={() => handleStatusChange(tab?.tab)}
           />
@@ -111,7 +117,7 @@ const ManageCourses = () => {
         <Loader color={"text-primary"} isBig={true} />
       ) : (
         <CreatedCourses
-          headingsData={["Courses", "Enrolled", "status"]}
+          headingsData={["Courses", "Enrolled", "Status"]}
           data={filteredData}
         />
       )}
