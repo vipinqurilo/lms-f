4a2364c87f4e8b90491c04d6ce0473f7803d6cfe
kdidrees <Khurshid@qurilo.com>
2025-02-18 15:33:39 +0530

    fixes

--- src/container/home/FeaturedCourses.jsx
+++ src/container/home/FeaturedCourses.jsx
@@ -42,7 +42,9 @@ export default function FeaturedCourses({ cardData = [] }) {
                 descWidth={"md:!w-2/3"}
               />
               <div className="md:block hidden">
-                <button>All Courses</button>
+                <button className="bg-orange-500 hover:bg-orange-600 text-white px-6 py-2 rounded-md shadow-md">
+                  All Courses
+                </button>
               </div>
             </div>
 
