ce48f17fed39d6da7d4ea67faaecc3591653eca5
kdidrees <Khurshid@qurilo.com>
2025-03-01 11:43:59 +0530

    added course request page

--- src/pages/admin-dashboard/approvals/courses/index.jsx
+++ src/pages/admin-dashboard/approvals/courses/index.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Course from "@/components/admin-dashboard/approvals/courses/Course";
+import TitleComp from "@/components/instructor/TitleComp";
 import ManageCourses from "@/container/admin-dashboard/ManageCourses";
 import StudentDashboardLayout from "@/layouts/student-dashboard/StudentDashboardLayout";
 import React from "react";
@@ -7,8 +9,16 @@ import React from "react";
 export default function index() {
   return (
     <StudentDashboardLayout>
-      <ManageCourses />
-
+      <main className="  p-10">
+        <div className="dashboard-container">
+        <TitleComp
+          heading={"Courses Approval Requests"}
+          des={"following are the approval requests for all the courses."}
+        />
+        <Course />
+        </div>
+   
+      </main>
     </StudentDashboardLayout>
   );
 }
