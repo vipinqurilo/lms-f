1c069b0dad0fbffb1a68020e937d02d9671abf12
Arjun850610 <arjun@qurilo.com>
2025-03-03 19:00:04 +0530

    Enhance MessageModal with maximize/minimize functionality and improve layout; adjust SupportDashboard width for better responsiveness

--- src/container/instructor/support/MessageModal.jsx
+++ src/container/instructor/support/MessageModal.jsx
@@ -4,7 +4,12 @@ import {
   updateAdminConversation,
   updateConversation,
 } from "@/store/slices/supportSlice";
-import { EllipsisVertical, SendHorizontal } from "lucide-react";
+import {
+  EllipsisVertical,
+  Maximize2,
+  Minimize2,
+  SendHorizontal,
+} from "lucide-react";
 import React, { useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { VscTriangleUp } from "react-icons/vsc";
@@ -14,8 +19,9 @@ import dateFormat from "dateformat";
 const MessageModal = ({ ticket, setMessages }) => {
   const { authUser } = useSelector((state) => state.user);
   const [newMessage, setNewMessage] = useState("");
-  const [isDropDown, setisDropDown] = useState(false);
+  const [isMaximized, setIsMaximized] = useState(false);
   const toggleIsDropDown = () => setisDropDown(!isDropDown);
+  const toggleMaximize = () => setIsMaximized(!isMaximized);
   const dispatch = useDispatch();
   const loading = useSelector(
     (state) => state.support.isLoading.updateConversation
@@ -83,11 +89,23 @@ const MessageModal = ({ ticket, setMessages }) => {
   };
 
   return (
-    <div className="w-80 lg:w-[30%] h-[calc(100vh-160px)] sticky top-5 flex flex-col border border-black/10 rounded-lg drop-shadow bg-white">
+    <div
+      className={`w-80 fixed flex flex-col border border-black/10 rounded-lg drop-shadow bg-white ${
+        isMaximized
+          ? "lg:w-[25%] h-[calc(100vh-160px)] top-24 z-[12] right-10"
+          : "lg:w-[30%] h-fit !bottom-20 !rounded-lg overflow-hidden z-[12] right-10"
+      }`}
+    >
       <div className="bg-secondary text-white p-3 font-semibold rounded-t-lg flex items-center justify-between">
         <h2>Conversations</h2>
 
         <div className="flex items-center gap-1">
+          <button
+            onClick={toggleMaximize}
+            className="text-white hover:text-gray-200"
+          >
+            {isMaximized ? <Minimize2 size={18} /> : <Maximize2 size={18} />}
+          </button>
           <button
             onClick={() => setMessages(null)}
             className="border border-black/10 rounded-full p-1 bg-secondary hover:bg-background text-white transition-custom "
@@ -136,83 +154,89 @@ const MessageModal = ({ ticket, setMessages }) => {
       </div>
 
       {/* Messages List */}
-      <div
-        className="flex-1 overflow-y-auto p-3 space-y-4 bg-gradient-to-b from-gray-100 via-white to-gray-50"
-        style={{
-          scrollbarWidth: "thin",
-          scrollbarColor: "#888 #f1f1f1",
-        }}
-      >
-        {ticket?.messages?.map((msg, index) => {
-          const isSender = msg?.sender?._id === authUser?._id;
-          return (
-            <div
-              key={index}
-              className={`flex flex-col ${
-                isSender ? "justify-end items-end" : "justify-start items-start"
-              }`}
-            >
-              <div
-                className={`p-2 max-w-[70%] text-wrap break-words rounded-lg text-sm ${
-                  isSender
-                    ? "bg-secondary/10 text-background rounded-br-none"
-                    : "bg-gray-200 text-black rounded-bl-none"
-                }`}
-              >
-                {msg.message}
-              </div>
-              {/* <span className="font-medium block text-xs text-gray-400 capitalize">
-                {isSender ? msg.sender : msg.receiver}
-              </span> */}
-              <span className="font-medium block text-xs text-gray-400">
-                {ticket?.createdAt
-                  ? getRelativeTime(ticket.createdAt)
-                  : "Just now"}
+      {isMaximized && (
+        <>
+          <div
+            className="flex-1 overflow-y-auto p-3 space-y-4 bg-gradient-to-b from-gray-100 via-white to-gray-50"
+            style={{
+              scrollbarWidth: "thin",
+              scrollbarColor: "#888 #f1f1f1",
+            }}
+          >
+            {ticket?.messages?.map((msg, index) => {
+              const isSender = msg?.sender?._id === authUser?._id;
+              return (
+                <div
+                  key={index}
+                  className={`flex flex-col ${
+                    isSender
+                      ? "justify-end items-end"
+                      : "justify-start items-start"
+                  }`}
+                >
+                  <div
+                    className={`p-2 max-w-[70%] text-wrap break-words rounded-lg text-sm ${
+                      isSender
+                        ? "bg-secondary/10 text-background rounded-br-none"
+                        : "bg-gray-200 text-black rounded-bl-none"
+                    }`}
+                  >
+                    {msg.message}
+                  </div>
+                  {/* <span className="font-medium block text-xs text-gray-400 capitalize">
+                  {isSender ? msg.sender : msg.receiver}
+                </span> */}
+                  <span className="font-medium block text-xs text-gray-400">
+                    {ticket?.createdAt
+                      ? getRelativeTime(ticket.createdAt)
+                      : "Just now"}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+
+          {ticket?.status === "Resolved" ? (
+            <div className="p-3 border-t flex items-center justify-between bg-green-100 text-green-800 rounded-b-lg">
+              <span className="flex items-center gap-2">
+                <svg
+                  className="w-5 h-5 text-green-600"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  ></path>
+                </svg>
+                <span className="text-sm font-medium">
+                  This ticket has been resolved
+                </span>
               </span>
             </div>
-          );
-        })}
-      </div>
+          ) : (
+            <div className="p-2 border-t flex items-center gap-2 bg-gray-100 rounded-b-lg">
+              <input
+                type="text"
+                placeholder="Type a message..."
+                className="flex-1 p-2 border rounded-lg outline-none text-sm"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                required
+              />
 
-      {ticket?.status === "Resolved" ? (
-        <div className="p-3 border-t flex items-center justify-between bg-green-100 text-green-800 rounded-b-lg">
-          <span className="flex items-center gap-2">
-            <svg
-              className="w-5 h-5 text-green-600"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M5 13l4 4L19 7"
-              ></path>
-            </svg>
-            <span className="text-sm font-medium">
-              This ticket has been resolved
-            </span>
-          </span>
-        </div>
-      ) : (
-        <div className="p-2 border-t flex items-center gap-2 bg-gray-100 rounded-b-lg">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            className="flex-1 p-2 border rounded-lg outline-none text-sm"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            required
-          />
-
-          <CommonButton
-            label={<SendHorizontal size={20} />}
-            onClick={handleSubmit}
-            loading={loading || adminLoading}
-          />
-        </div>
+              <CommonButton
+                label={<SendHorizontal size={20} />}
+                onClick={handleSubmit}
+                loading={loading || adminLoading}
+              />
+            </div>
+          )}
+        </>
       )}
     </div>
   );
