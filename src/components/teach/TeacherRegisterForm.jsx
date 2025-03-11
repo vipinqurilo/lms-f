9132c2bbf6ce0590f4c72d6b2dc1a4acad51e7e8
amankumar36 <vipin@qurilo.com>
2025-03-11 17:18:06 +0530

    fix

--- src/components/teach/TeacherRegisterForm.jsx
+++ src/components/teach/TeacherRegisterForm.jsx
@@ -46,7 +46,7 @@ const TeacherRegisterForm = () => {
       <div className="bg-white/40 backdrop-blur-sm text-white p-8 md:w-full rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-2">Apply To Teach</h2>
         <p className="mb-4">Teach what you are passionate about</p>
-
+        
         {authUser !== null ? (
           <>
             {authUser.role === "admin" ? (
--- src/store/api/api.js
+++ src/store/api/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const baseUrlAshokSir = "https://6g2n7ff0-8000.inc1.devtunnels.ms/api";
 const baseUrlAbhiSir = "https://q1ztd2hs-8000.inc1.devtunnels.ms/api";
-const baseURLVipin = "https://rvdr9qkh-8000.inc1.devtunnels.ms/api";
+const baseURLVipin = "  ";
 const verceUrl="https://lms-backend-rho-pink.vercel.app/api"
 const baseUrlKD = "https://wq1jbb9k-8000.inc1.devtunnels.ms/api";
 
 export const api = axios.create({
-  baseURL: baseUrlKD,
-  // baseURL: baseUrlAbhiSir,
+  // baseURL: baseUrlKD,
+  baseURL: baseURLVipin,
   headers: {
     "Cache-Control": "no-store, no-cache,   must-revalidate, proxy-revalidate",
     Pragma: "no-cache",
