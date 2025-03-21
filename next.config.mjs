e01b652864456680b15c4941f8887c2365748eba
qurilovpn <165997344+qurilosolution@users.noreply.github.com>
2025-03-21 16:51:15 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,9 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://wq1jbb9k-8000.inc1.devtunnels.ms/api/:path*", // Correctly forwards the path
+        // destination: "https://wq1jbb9k-8000.inc1.devtunnels.ms/api/:path*", // Correctly forwards the path
+        destination: "https://lms-backend-rho-pink.vercel.app/api/:path*", // Correctly forwards the path
+
       },
     ];
   },
