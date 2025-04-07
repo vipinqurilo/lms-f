1d80a8a865aac082828b514d00e5c275016db888
qurilovpn <165997344+qurilosolution@users.noreply.github.com>
2025-04-07 11:56:10 +0530

    fixed the multiple verification api hit in course payfast module and some minor fixes in ui

--- src/components/instructor/BackgroundModal.jsx
+++ src/components/instructor/BackgroundModal.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const BackgroundModal = ({ PropComponent }) => {
+  useEffect(() => {
+    // Disable scrolling when modal is mounted
+    document.body.style.overflow = 'hidden';
+    
+    // Re-enable scrolling when modal is unmounted
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, []);
+
   return (
     <div className="w-full h-screen flex items-center justify-center bg-black/25 !z-[20] !fixed !top-0 !left-0 backdrop-blur-sm">
       {typeof PropComponent === "function" ? <PropComponent /> : PropComponent}
